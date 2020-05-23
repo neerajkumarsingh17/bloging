4ae7ca74b0c306fb52175408423557071eb83995
Neeraj <nk7380@gmial.com>
2020-05-23 23:24:52 +0530

    media file add gitignore

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    import pdb; pdb.set_trace()
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 @login_required
--- bloging/settings.py
+++ bloging/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'bloging.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bloging',
-        'USER': 'bloging',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bloging',
+#         'USER': 'bloging',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
